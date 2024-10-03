815c4049cdf7dcfa38e8d2f672d765fbd07ee0b7
BetoGmez <luisrg1402@gmail.com>
2024-10-03 14:47:41 -0600

    comentario

---
+++ unidad1/pruebas/U1Ejer01_LuisAlbertoRomeroGomez.py
@@ -0,0 +1,33 @@
+'''
+Descripción: Fundamentos de Python
+Nombre: Luis Alberto Romero Gómez
+Fecha: 03/10/24
+Tema: 
+'''
+# 1. Escribe la instrucción para desplegar el título
+print("Ejercicio 1 – Fundamentos de Python")
+# 2. Escribe la instrucción para desplegar el título “Autor – [Nombre del participante]”
+print("Autor - Luis Alberto Romero Gómez")
+# 3. Escribe la instrucción para realizar un salto de línea
+print()
+# 4. Declara una variable de tipo real y asigna el valor 4
+variable1 = 8
+# 5. Declara una variable de tipo real y asigna el valor 3
+variable2 = 2
+# 6. Imprime el contenido de las dos variables
+print("El valor de la variable1 es:", variable1)
+print("El valor de la variable2 es:", variable2)
+# 7. Escribe la instrucción para realizar un salto de línea
+print()
+# 8. Haz el cálculo de la suma y asígnala a una variable llamada suma; haz lo mismo para
+# 9. las operaciones de resta, multiplicación, división y potenciación
+suma = variable1 + variable2
+resta = variable1 - variable2
+multiplicacion = variable1 * variable2
+division = variable1 / variable2
+potencia = variable1 ** variable2
+print("Suma: ", suma)
+print("Resta: ", resta)
+print("Multiplicacion: ", multiplicacion)
+print("Division: ", division)
+print("Potencia: ", potencia)
